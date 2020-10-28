bbd34b603b85796f3a3406495a0269b4e4ef25f3
EfranH25 <60274368+EfranH25@users.noreply.github.com>
2020-10-28 18:10:00 -0400

    Add files via upload

--- Newegg Scrapper.py
+++ Newegg Scrapper.py
@@ -1,8 +1,8 @@
 import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
-import scrapy
 import pandas as pd
+
 
 # This function is incomplete
 # Looks at product page and extracts more granular information
@@ -18,7 +18,8 @@ def item_page(my_url):
 
     # gather specific details on item
     price = page_soup.findAll('li', {'class': 'price-current'})[0].text
-    written_reviews_container =page_soup.findAll('div', {'class':'rating rating-%'})
+    written_reviews_container = page_soup.findAll('div', {'class': 'rating rating-%'})
+
 
 # Looks through newegg search result page and extracts relevent information
 # Information extracted: name, brand, price, rating, shipping, url
@@ -33,13 +34,12 @@ def build_table(my_url, model):
 
     # looks through entire page to find containers that hold GPU info
     containers = page_soup.findAll('div', {'class': 'item-container'})
-    #print(containers)
+    # print(containers)
 
     # the first 4 items are recommended advertisement items --> so we skip them
     containers = containers[4:len(containers)]
 
-
-    headers = ['brand', 'model', 'product_name', 'price',  'rating', 'shipping', 'url']
+    headers = ['brand', 'model', 'product_name', 'price', 'rating', 'shipping', 'url']
     df = pd.DataFrame(columns=headers)
 
     # loop through each GPU in container and gather data
@@ -48,7 +48,7 @@ def build_table(my_url, model):
         container_tag_list = [tag.name for tag in container.div.div.find_all()]
 
         if 'img' in container_tag_list:
-            #print(container)
+            # print(container)
 
             brand = container.div.div.a.img['title'].split()[0]
 
@@ -70,10 +70,12 @@ def build_table(my_url, model):
             price_container = container.findAll('li', {'class', 'price-current'})
             product_price = price_container[0].text
 
-            new_rows = pd.Series([brand, model, product_name, product_price, rating, shipping, product_url], index=df.columns)
+            new_rows = pd.Series([brand, model, product_name, product_price, rating, shipping, product_url],
+                                 index=df.columns)
             df = df.append(new_rows, ignore_index=True)
 
     return df
+
 
 if __name__ == '__main__':
     pd.set_option('display.max_columns', None)
@@ -81,8 +83,7 @@ if __name__ == '__main__':
 
     # set the number of pages for your URL
 
-
-    headers = ['brand', 'model', 'product_name', 'price',  'rating', 'shipping', 'url']
+    headers = ['brand', 'model', 'product_name', 'price', 'rating', 'shipping', 'url']
     master_df = pd.DataFrame(columns=headers)
 
     print('Enter GPU Model (NOTE: Should be the name of the item searched Newegg search bar)')
@@ -92,16 +93,18 @@ if __name__ == '__main__':
     print('Max digit for number of pages to scrape')
     max_page = int(input())
 
-    print('Enter URL (NOTE: Make sure page is at the very end of the URL without a number to the page traversal to work properly)')
+    print(
+        'Enter URL (NOTE: Make sure page is at the very end of the URL without a number to the page traversal to work properly)')
     print('EXAMPLE URL for GTX 1080: https://www.newegg.com/p/pl?d=GTX+1080&N=100007709&isdeptsrh=1&PageSize=96&page=')
     url = input()
 
-    for i in range(1, max_page+1):
-        url = url+str(i)
+    for i in range(1, max_page + 1):
+        url = url + str(i)
         df = build_table(url, GPU_model)
 
         master_df = pd.concat([master_df, df])
 
-    #item_page('https://www.newegg.com/evga-geforce-gtx-1080-ti-11g-p4-6591-kr/p/N82E16814487376?Description=GTX')
-    master_df.to_csv('products_'+GPU_model + '.csv')
+    file_name = 'products_' + GPU_model + '.csv'
+    # item_page('https://www.newegg.com/evga-geforce-gtx-1080-ti-11g-p4-6591-kr/p/N82E16814487376?Description=GTX')
+    master_df.to_csv(f'{file_name}.csv')
     print('done')
